ed45ee1c8924ac79eec3b871802c2ec81278b75b
Taikhoom Attar <tattar8@me.com>
2023-04-05 03:58:56 -0400

    Initial Commit

---
+++ Mute and Send/Mute_and_SendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mute_and_SendApp.swift
+//  Mute and Send
+//
+//  Created by Taikhoom Attar on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Mute_and_SendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
